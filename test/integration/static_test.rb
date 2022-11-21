363235103a38bce6f9f7c68be3927beb7c730a56
Spencer Norman <spencerdnorman@gmail.com>
2022-11-21 15:14:10 -0600

    removed unneeded test

--- test/integration/static_test.rb
+++ test/integration/static_test.rb
@@ -6,9 +6,9 @@ class Jumpstart::StaticTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "dashboard" do
-    sign_in users(:one)
-    get root_path
-    assert_select "h1", I18n.t("dashboard.show.title")
-  end
+  # test "dashboard" do
+  #   sign_in users(:one)
+  #   get root_path
+  #   assert_select "h1", I18n.t("dashboard.show.title")
+  # end
 end
