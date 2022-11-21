e3f66028e821819bcddf1a47de04651186dc287f
Spencer Norman <spencerdnorman@gmail.com>
2022-11-21 15:09:28 -0600

    lint fixes

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -90,7 +90,7 @@ class UserDashboard < Administrate::BaseDashboard
     :name,
     :email,
     :admin,
-    :terms_of_service,
+    :terms_of_service
   ].freeze
 
   # Overwrite this method to customize how users are displayed
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -28,9 +28,8 @@
 #  updated_at              :datetime         not null
 #
 class Appointment < ApplicationRecord
-
   # Broadcast changes in realtime with Hotwire
-  after_create_commit  -> { broadcast_prepend_later_to :appointments, partial: "appointments/index", locals: { appointment: self } }
-  after_update_commit  -> { broadcast_replace_later_to self }
+  after_create_commit -> { broadcast_prepend_later_to :appointments, partial: "appointments/index", locals: {appointment: self} }
+  after_update_commit -> { broadcast_replace_later_to self }
   after_destroy_commit -> { broadcast_remove_to :appointments, target: dom_id(self, :index) }
 end
