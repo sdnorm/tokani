93570869fd785b751effb79637875c1372fbbf20
Spencer Norman <spencerdnorman@gmail.com>
2022-11-29 00:30:15 -0600

    interpreter is brought to interpreter detail path on sign up

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def show
+    if current_account_user.interpreter? #&& current_user.interpreter_profile.nil?
+      redirect_to new_interpreter_detail_path
+    end
   end
 end
