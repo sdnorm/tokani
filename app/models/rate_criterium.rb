699c72ea269fc21c7e8e534f1c52275b3809973e
Shanti Braford <shantibraford@gmail.com>
2022-12-02 22:19:51 +0000

    standardrb fixes

--- app/models/rate_criterium.rb
+++ app/models/rate_criterium.rb
@@ -1,8 +1,7 @@
 class RateCriterium < ApplicationRecord
   belongs_to :account
 
-  enum type_key: { sites_departments: 0, specialty: 1, language: 2, interpreter_type: 3 }
+  enum type_key: {sites_departments: 0, specialty: 1, language: 2, interpreter_type: 3}
 
   scope :sorted, -> { order(sort_order: :asc) }
-
 end
