1d9764d6a4383b10c59e4bdc31b22ef5949f4018
Spencer Norman <spencerdnorman@gmail.com>
2022-12-07 13:55:45 -0600

    migrate updates

--- test/models/interpreter_detail_test.rb
+++ test/models/interpreter_detail_test.rb
@@ -7,13 +7,10 @@
 #  city                    :string
 #  dob                     :date
 #  drivers_license         :string
-#  email                   :string
 #  emergency_contact_name  :string
 #  emergency_contact_phone :string
-#  fname                   :string
 #  gender                  :integer
 #  interpreter_type        :integer
-#  lname                   :string
 #  primary_phone           :string
 #  ssn                     :string
 #  start_date              :date
@@ -26,6 +23,10 @@
 # Indexes
 #
 #  index_interpreter_details_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 require "test_helper"
 
