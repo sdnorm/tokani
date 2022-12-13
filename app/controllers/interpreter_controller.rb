9c80ae5862f48ced3cf8373f242b52d14c8fbed6
Spencer Norman <spencerdnorman@gmail.com>
2022-12-13 12:50:44 -0600

    lint fix

--- app/controllers/interpreter_controller.rb
+++ app/controllers/interpreter_controller.rb
@@ -2,7 +2,7 @@ class InterpreterController < ApplicationController
   include CurrentHelper
 
   def index
-    current_account_id = current_account.id
+    # current_account_id = current_account.id
     @interpreter_account_ids = AccountUser.where(roles: {interpreter: true}).where(account_id: current_account.id).pluck(:user_id)
     # @interpreter_account_ids = AccountUser.where("roles->>'interpreter' = 'true'").where(account_id: current_account_id).pluck(:user_id)
     @interpreters_all = User.includes(:interpreter_detail).where(id: @interpreter_account_ids)
