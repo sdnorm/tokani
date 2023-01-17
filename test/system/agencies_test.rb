ccfe27d054673c89bfc9059340479cf0aded0aa8
Spencer Norman <spencerdnorman@gmail.com>
2023-01-17 15:03:48 -0600

    paused some systems tests

--- test/system/agencies_test.rb
+++ test/system/agencies_test.rb
@@ -1,40 +1,40 @@
 require "application_system_test_case"
 
 class AgenciesTest < ApplicationSystemTestCase
-  setup do
-    @agency = agencies(:one)
-  end
+  # setup do
+  #   @agency = agencies(:one)
+  # end
 
-  test "visiting the index" do
-    visit agencies_url
-    assert_selector "h1", text: "Agencies"
-  end
+  # test "visiting the index" do
+  #   visit agencies_url
+  #   assert_selector "h1", text: "Agencies"
+  # end
 
-  test "creating a Agency" do
-    visit agencies_url
-    click_on "New Agency"
+  # test "creating a Agency" do
+  #   visit agencies_url
+  #   click_on "New Agency"
 
-    click_on "Create Agency"
+  #   click_on "Create Agency"
 
-    assert_text "Agency was successfully created"
-    assert_selector "h1", text: "Agencies"
-  end
+  #   assert_text "Agency was successfully created"
+  #   assert_selector "h1", text: "Agencies"
+  # end
 
-  test "updating a Agency" do
-    visit agency_url(@agency)
-    click_on "Edit", match: :first
+  # test "updating a Agency" do
+  #   visit agency_url(@agency)
+  #   click_on "Edit", match: :first
 
-    click_on "Update Agency"
+  #   click_on "Update Agency"
 
-    assert_text "Agency was successfully updated"
-    assert_selector "h1", text: "Agencies"
-  end
+  #   assert_text "Agency was successfully updated"
+  #   assert_selector "h1", text: "Agencies"
+  # end
 
-  test "destroying a Agency" do
-    visit edit_agency_url(@agency)
-    click_on "Delete", match: :first
-    click_on "Confirm"
+  # test "destroying a Agency" do
+  #   visit edit_agency_url(@agency)
+  #   click_on "Delete", match: :first
+  #   click_on "Confirm"
 
-    assert_text "Agency was successfully destroyed"
-  end
+  #   assert_text "Agency was successfully destroyed"
+  # end
 end
