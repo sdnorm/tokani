bcd214ce9818cb41066099175cf2b19cf5cec4f4
Spencer Norman <spencerdnorman@gmail.com>
2023-01-17 14:43:32 -0600

    tokani admin can create an agency

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -3,6 +3,7 @@
 # Table name: accounts
 #
 #  id                 :uuid             not null, primary key
+#  agency             :boolean
 #  customer           :boolean          default(FALSE)
 #  domain             :string
 #  extra_billing_info :text
