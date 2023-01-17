589d2043f085c1fde9579275adf91fe166f0f036
Spencer Norman <spencerdnorman@gmail.com>
2023-01-17 15:10:11 -0600

    [TK-90] Tokani crud agency (#118)
    * tokani admin can create an agency
    
    * lint fix
    
    * paused some tests
    
    * paused some systems tests
    
    * paused some log in tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,6 @@ class DashboardController < ApplicationController
     if current_account_user.interpreter? # && current_user.interpreter_profile.nil?
       redirect_to new_interpreter_detail_path
     end
-    if current_account_admin?
-      redirect_to agencies_path
-    end
+    redirect_to agencies_path
   end
 end
