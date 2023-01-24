a2322c138e468ce18d6bc30a2940d93bda88c310
Spencer Norman <spencerdnorman@gmail.com>
2023-01-23 22:19:37 -0600

    moving aothorization logic to dashboard show controller for root page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,16 @@ class DashboardController < ApplicationController
   def show
     if current_account_user.interpreter? # && current_user.interpreter_profile.nil?
       redirect_to new_interpreter_detail_path
+    elsif current_account_user.agency_admin?
+      render "dashboard/agency_admin"
+    elsif current_account_user.agency_admin?
+      render "agency/index"
+    else
+      render "dashboard/requestor_details"
     end
     redirect_to agencies_path
   end
+  # <% elsif current_account_user.admin? %>
+  #   <%= render partial: "tokani_admin" %>
+  # <% else %>
 end
