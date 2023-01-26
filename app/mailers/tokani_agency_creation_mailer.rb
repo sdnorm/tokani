94839c01ff6290a81e693958e46203dbe2ec5c26
Spencer Norman <spencerdnorman@gmail.com>
2023-01-26 15:27:00 -0600

    lint fixes

--- app/mailers/tokani_agency_creation_mailer.rb
+++ app/mailers/tokani_agency_creation_mailer.rb
@@ -1,5 +1,4 @@
 class TokaniAgencyCreationMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -17,10 +16,10 @@ class TokaniAgencyCreationMailer < ApplicationMailer
   private
 
   def create_reset_password_token(user)
-      raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
-      @token = raw
-      user.reset_password_token = hashed
-      user.reset_password_sent_at = Time.now.utc
-      user.save
+    raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
+    @token = raw
+    user.reset_password_token = hashed
+    user.reset_password_sent_at = Time.now.utc
+    user.save
   end
 end
--- test/mailers/previews/tokani_agency_creation_mailer_preview.rb
+++ test/mailers/previews/tokani_agency_creation_mailer_preview.rb
@@ -1,9 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/tokani_agency_creation_mailer
 class TokaniAgencyCreationMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/tokani_agency_creation_mailer/welcome
   def welcome
     TokaniAgencyCreationMailer.welcome
   end
-
 end
