087a141804f2f32d744fd79d0e6faa3dcf446de3
Shanti Braford <shantibraford@gmail.com>
2023-01-27 07:42:53 +0000

    fix schema.rb merge issue

--- db/schema.rb
+++ db/schema.rb
@@ -545,6 +545,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_051624) do
     t.boolean "is_processed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "providers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "last_name"
