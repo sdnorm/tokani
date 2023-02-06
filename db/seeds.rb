499686199ee325e46bc97d2e713247b2095bc81b
Spencer Norman <spencerdnorman@gmail.com>
2023-02-06 15:31:51 -0600

    updated the seed data to make sure it creates an account user for super admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -83,3 +83,5 @@ end
 
 # Super Admin
 User.create!(name: "Super Admin", email: "super@admin.com", password: "password", password_confirmation: "password", admin: true, terms_of_service: true)
+
+AccountUser.create!(account_id: Account.find_by(name: "Super Admin").id, user_id: User.find_by(email: "super@admin.com").id, roles: {"admin" => true})
