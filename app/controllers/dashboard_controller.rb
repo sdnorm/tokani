70149c13bf686abe7cb33a164507b2c699e840b9
Neda Wo <neda@gofluently.com>
2023-02-16 19:56:31 -0800

    int sidenav in

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
   def show
     if current_account_user.interpreter? # && current_user.interpreter_profile.nil?
-      redirect_to new_interpreter_detail_path
+      # redirect_to new_interpreter_detail_path
+      render template: "dashboard/interpreter"
     elsif current_account_user.agency_admin?
       render template: "dashboard/agency_admin"
     elsif current_account_user.admin?
