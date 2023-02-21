9ff55a2868df428f15f49c0c74d7e43073758809
Spencer Norman <spencerdnorman@gmail.com>
2023-02-21 12:30:44 -0600

    root page fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def show
-    if current_account_user.interpreter? # && current_user.interpreter_profile.nil?
+    if current_account_user.nil?
+      render template: "dashboard/general"
+    elsif current_account_user.interpreter? # && current_user.interpreter_profile.nil?
       # redirect_to new_interpreter_detail_path
       render template: "dashboard/interpreter"
     elsif current_account_user.agency_admin?
