75c1dd53b57861024ba0a6226081e6408eb573bb
Spencer Norman <spencerdnorman@gmail.com>
2023-02-21 11:43:33 -0600

    trying a fix for dashboard not working

--- config/routes.rb
+++ config/routes.rb
@@ -84,7 +84,7 @@ Rails.application.routes.draw do
       end
       resources :accounts
       resources :account_users
-      resources :agency_details
+      # resources :agency_details
       resources :customer_details
       resources :plans
       namespace :pay do
