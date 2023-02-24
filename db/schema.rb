27f01bf94afffa6cb93b8bb8bdd233afdd288626
Spencer Norman <spencerdnorman@gmail.com>
2023-02-24 09:17:40 -0600

    schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_205801) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_043110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_205801) do
     t.string "billing_email"
     t.boolean "is_active", default: true
     t.boolean "agency"
-    t.string "billing_email"
     t.integer "account_users_count", default: 0
     t.index ["created_at"], name: "index_accounts_on_created_at"
     t.index ["owner_id"], name: "index_accounts_on_owner_id"
