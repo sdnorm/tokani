e88607ca5d052cce89981b0a605e24b8a6bed7bc
nedak-fluency <93293149+nedak-fluency@users.noreply.github.com>
2023-03-01 20:14:14 -0800

    Small screenshare fixes (#230)
    * small fixes during screenshare
    
    * standardrb fix
    
    * small_screenshare_fixes to main
    
    ---------
    
    Co-authored-by: Spencer Norman <spencerdnorman@gmail.com>

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -79,7 +79,7 @@ class AgenciesController < ApplicationController
     @agency = Agency.new(agency_params) # .merge(agency: true)
     respond_to do |format|
       if @agency.save
-        agency.update(agency: true)
+        @agency.update(agency: true)
         CreateAgencyOwnerUserAccountJob.perform_later(@agency.id)
         format.html { redirect_to @agency, notice: "Agency was successfully created." }
         format.json { render :show, status: :created, location: @agency }
@@ -148,6 +148,7 @@ class AgenciesController < ApplicationController
       ],
       agency_detail_attributes: [
         :id,
+        :company_website,
         :primary_contact_first_name,
         :primary_contact_last_name,
         :primary_contact_email,
@@ -161,6 +162,7 @@ class AgenciesController < ApplicationController
         :phone_number,
         :url,
         time_zones: []
+
       ]
     )
 
