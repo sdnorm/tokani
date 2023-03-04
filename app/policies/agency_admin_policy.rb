ef4a65bf939d7c301220e059065481ff4b6256c6
Jose Stephen Peña <jose.pena@cybersoftbpo.com>
2023-03-04 17:32:58 +0800

    TK-223 Fix Sidenav Behavior for User Types
    - Create interpreter & agency_admin pundit policies
    - Fix interpreter & shared sidenav rendering/overlaying on other pages
    - Fix agency_detail_form errors

---
+++ app/policies/agency_admin_policy.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class AgencyAdminPolicy < ApplicationPolicy
+  def show_side_nav? controller_name
+    agency_admin? && blacklisted_controllers.exclude?(controller_name)
+  end
+
+  private
+
+  # hide sidenav only from these controllers
+  def blacklisted_controllers
+    %w[
+      registrations accounts subscriptions passwords
+    ]
+  end
+
+  def agency_admin?
+    @account_user.agency_admin?
+  end
+end
--- test/controllers/customer_categories_controller_test.rb
+++ test/controllers/customer_categories_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class CustomerCategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @customer_category = customer_categories(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test "should get index" do
