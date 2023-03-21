a79b4b292c3089309cc7b2da50f33a7e460f149c
Neda Wo <neda@gofluently.com>
2023-03-21 09:29:22 -0700

    standardrb

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,6 @@ class User < ApplicationRecord
 
   has_many :appointment_statuses, dependent: :destroy, foreign_key: :user_id
 
-
   # has_many :pay_rate_interpreters, dependent: :destroy
   # has_many :pay_rates, through: :pay_rate_interpreters
 
