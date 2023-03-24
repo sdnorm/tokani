a00374f67fdcf7c86cd158b4a53dc66fd848509a
Spencer Norman <spencerdnorman@gmail.com>
2023-03-23 22:11:00 -0500

    Int finish apt fix (#297)
    * pay/bill rate fixes
    
    * lint fix
    
    * added int_id foreign key for pay_rate_interpreters
    
    ---------
    
    Co-authored-by: Shanti Braford <shantibraford@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,8 +85,8 @@ class User < ApplicationRecord
 
   has_many :appointment_statuses, dependent: :destroy, foreign_key: :user_id
 
-  # has_many :pay_rate_interpreters, dependent: :destroy
-  # has_many :pay_rates, through: :pay_rate_interpreters
+  has_many :pay_rate_interpreters, dependent: :destroy, foreign_key: :interpreter_id
+  has_many :pay_rates, through: :pay_rate_interpreters
 
   has_many :availabilities
 
