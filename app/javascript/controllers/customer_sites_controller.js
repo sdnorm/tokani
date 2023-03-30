ab9f478749e5531db950b1b701b2746b90b0ba92
Shanti Braford <shantibraford@gmail.com>
2023-03-29 20:28:39 -0700

    Wire up Accounting pages to already-implemented actions/views (#315)
    * Wire up Accounting pages to already-implemented actions/views
    
    * remove some javascript console logging from stimulus controllers

--- app/javascript/controllers/customer_sites_controller.js
+++ app/javascript/controllers/customer_sites_controller.js
@@ -5,23 +5,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["sites", "departments", "checkbox"]
 
-  connect() {
-    console.log("customer sites controller connected...")
-  }
-
   customerSelected(event) {
-    console.log("customerSelected called", event)
-
     let agency_customer_ids = []
 
     this.checkboxTargets.forEach((child) => {
       if (child.checked) {
-        console.log("child ", child)
-        console.log("is checked")
         agency_customer_ids.push(child.value)
       }
     })
-    console.log("agency_customer_ids: " + agency_customer_ids)
 
     this.sitesTarget.classList.remove('hidden')
 
@@ -41,16 +32,11 @@ export default class extends Controller {
   siteSelected(event) {
     let site_ids = this.getSelectValues(event.target)
 
-    console.log("Site IDs: ")
-    console.log(site_ids)
-
     this.departmentsTarget.classList.remove('hidden')
 
-    console.log("fetching: " + '/sites/departments_dropdown_for_reports?site_ids=' + site_ids)
     fetch('/sites/departments_dropdown_for_reports?site_ids=' + site_ids)
       .then(response => response.text())
       .then(html => this.departmentsTarget.innerHTML = html)
-
   }
 
   getSelectValues(select) {
