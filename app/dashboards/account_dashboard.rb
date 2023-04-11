9d6f4ee5d0383a49ce38ddefa0d2f93ac947e047
Spencer Norman <spencerdnorman@gmail.com>
2023-04-11 15:03:15 -0500

    updated some dashbaord

--- app/dashboards/account_dashboard.rb
+++ app/dashboards/account_dashboard.rb
@@ -16,14 +16,55 @@ class AccountDashboard < Administrate::BaseDashboard
     users: Field::HasMany,
     avatar: Field::ActiveStorage,
     id: Field::String,
-    name: Field::String,
-    personal: Field::Boolean,
+    account_invitations: Field::HasMany,
+    account_languages: Field::HasMany,
+    account_sites: Field::HasMany,
+    account_users: Field::HasMany,
+    account_users_count: Field::Number,
+    addresses: Field::HasMany,
+    agencies: Field::HasMany,
     agency: Field::Boolean,
+    agency_customers: Field::HasMany,
+    agency_detail: Field::HasOne,
+    agency_recipients: Field::HasMany,
+    appointments: Field::HasMany,
+    bill_rates: Field::HasMany,
+    billing_address: Field::HasOne,
+    billing_email: Field::String,
+    charges: Field::HasMany,
+    checklist_types: Field::HasMany,
+    customer: Field::Boolean,
+    customer_agencies: Field::HasMany,
+    customer_appointments: Field::HasMany,
+    customer_detail: Field::HasOne,
+    customers: Field::HasMany,
+    domain: Field::String,
+    extra_billing_info: Field::Text,
+    is_active: Field::Boolean,
+    languages: Field::HasMany,
+    name: Field::String,
+    notification_email: Field::HasOne,
+    notifications: Field::HasMany,
+    owner: Field::BelongsTo,
+    pay_customers: Field::HasMany,
+    pay_rates: Field::HasMany,
+    payment_processor: Field::HasOne,
+    personal: Field::Boolean,
+    physical_address: Field::HasOne,
+    process_batches: Field::HasMany,
+    providers: Field::HasMany,
+    rate_criteria: Field::HasMany,
+    recipients: Field::HasMany,
+    reports: Field::HasMany,
+    requestor_details: Field::HasMany,
+    shipping_address: Field::HasOne,
+    sites: Field::HasMany,
+    specialties: Field::HasMany,
+    subdomain: Field::String,
+    subscriptions: Field::HasMany,
+    users: Field::HasMany,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    extra_billing_info: Field::Text,
-    customer: Field::Boolean
-    # agen: Field::String
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -70,6 +111,18 @@ class AccountDashboard < Administrate::BaseDashboard
     :personal,
     :extra_billing_info
   ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
 
   # Overwrite this method to customize how accounts are displayed
   # across all pages of the admin dashboard.
--- test/models/checklist_type_test.rb
+++ test/models/checklist_type_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: checklist_types
+#
+#  id                  :bigint           not null, primary key
+#  format              :integer
+#  is_active           :boolean          default(TRUE), not null
+#  name                :string
+#  requires_expiration :boolean
+#  requires_upload     :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  account_id          :uuid             not null
+#
+# Indexes
+#
+#  index_checklist_types_on_account_id  (account_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
+#
 require "test_helper"
 
 class ChecklistTypeTest < ActiveSupport::TestCase
