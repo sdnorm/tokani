822e7d90567951577217413a581b80d7ddf054d3
Spencer Norman <spencerdnorman@gmail.com>
2023-04-13 23:21:45 -0500

    lint fix

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,12 +3,12 @@ Rails.application.reloader.to_prepare do
   ActionMailer::Base.default_options = {from: Jumpstart.config.default_from_email}
 
   if Rails.env.production?
-    ActionMailer::Base.default_url_options[:host] = "app.tokani.com" #Jumpstart.config.domain
+    ActionMailer::Base.default_url_options[:host] = "app.tokani.com" # Jumpstart.config.domain
     ActionMailer::Base.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
   end
 
   if Rails.env.staging?
-    ActionMailer::Base.default_url_options[:host] = "stage.tokani.com" #Jumpstart.config.domain
+    ActionMailer::Base.default_url_options[:host] = "stage.tokani.com" # Jumpstart.config.domain
     ActionMailer::Base.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
   end
 end
