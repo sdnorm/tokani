2ee98792aae74ae505f93b7290f0c66c18b9bb26
Spencer Norman <spencerdnorman@gmail.com>
2023-04-13 23:29:29 -0500

    Interpreter mailer fixing (#358)
    * interpreter mailer fixes and setting url for mailer views
    
    * lint fix

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,13 @@
 Rails.application.reloader.to_prepare do
   ActionMailer::Base.default_options = {from: Jumpstart.config.default_from_email}
 
-  if Rails.env.production? || Rails.env.staging?
-    ActionMailer::Base.default_url_options[:host] = Jumpstart.config.domain
+  if Rails.env.production?
+    ActionMailer::Base.default_url_options[:host] = "app.tokani.com" # Jumpstart.config.domain
+    ActionMailer::Base.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
+  end
+
+  if Rails.env.staging?
+    ActionMailer::Base.default_url_options[:host] = "stage.tokani.com" # Jumpstart.config.domain
     ActionMailer::Base.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
   end
 end
