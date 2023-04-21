89475bdaff327591157511672658dc86a591db8f
Spencer Norman <spencerdnorman@gmail.com>
2023-04-21 09:06:37 -0500

    commented out migration to update user time zone data, range the method on the live console

--- db/migrate/20230418032939_fix_invalid_time_zones.rb
+++ db/migrate/20230418032939_fix_invalid_time_zones.rb
@@ -1,10 +1,10 @@
 class FixInvalidTimeZones < ActiveRecord::Migration[7.0]
-  def change
-    User.all.each do |u|
-      if u.time_zone&.include?("GMT-")
-        # If they have an invalid time zone, just set it to Pacific Time
-        u.update(time_zone: "Pacific Time (US & Canada)")
-      end
-    end
-  end
+  # def change
+  #   User.all.each do |u|
+  #     if u.time_zone&.include?("GMT-")
+  #       # If they have an invalid time zone, just set it to Pacific Time
+  #       u.update(time_zone: "Pacific Time (US & Canada)")
+  #     end
+  #   end
+  # end
 end
