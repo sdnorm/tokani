3e6eb98fa0be5a11317f6429858ed0608baa60a4
Shanti Braford <shantibraford@gmail.com>
2023-05-05 03:39:43 +0000

    lint fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -188,10 +188,10 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  if Rails.env.development?
-    config.timeout_in = 365.days
+  config.timeout_in = if Rails.env.development?
+    365.days
   else
-    config.timeout_in = 15.minutes
+    15.minutes
   end
 
   # ==> Configuration for :lockable
