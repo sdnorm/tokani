5b76cf2c6806b193ce3664ed0f283cb798021e4c
Shanti Braford <shantibraford@gmail.com>
2023-05-09 07:14:48 -0700

    TK-265 - Fix Edit Appointment issue with general/specific radio buttons (#519)

--- app/javascript/controllers/radio_button_controller.js
+++ app/javascript/controllers/radio_button_controller.js
@@ -1,9 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
-
 export default class extends Controller {
-  // static values = { message: String};
-  static targets = ["fieldset"]
+  static targets = ["fieldset", "typeselected"]
+
+  // This method is very specific to the Edit Appointment page. Like the comment below says,
+  // this could be moved to a dedicated controller if we need a more generic radio button controller.
+  connect() {
+    const selectedFieldSet = this.typeselectedTarget
+
+    this.fieldsetTargets.forEach( fs => {
+      if (fs.id == this.typeselectedTarget.value) {
+        fs.disabled = false
+        fs.querySelectorAll('div.multiselect__container').forEach(so => {so.disabled = false })
+        fs.querySelectorAll('input').forEach(so => {so.disabled = false })
+        fs.querySelectorAll('.selected-option', 'div.input-group').forEach(so => {so.classList.remove('disabled') })
+      } else {
+        fs.disabled = true
+        fs.querySelectorAll('div.multiselect__container').forEach(so => {so.disabled = true })
+        fs.querySelectorAll('input').forEach(so => {so.disabled = true })
+        fs.querySelectorAll('input').forEach(so => {so.value = "" })
+        fs.querySelectorAll('.selected-option').forEach(so => {so.classList.add('disabled')})
+      }
+    })
+  }
 
   selectFieldset(event) {
     
@@ -27,6 +46,7 @@ export default class extends Controller {
     this.clearFields(event)
 
   }
+
   //This is super specific for the default and language selections on pay and bill rates 
   //May need to move to own controller at some point or re-work 
   clearFields(event) {
